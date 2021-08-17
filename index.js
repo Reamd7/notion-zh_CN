ecc0d40e10a152d0324f87d2f8067a81236e2265
reamd7 <reamd7@gmail.com>
2021-08-17 12:52:33 +0800

    feat: init npm package

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const axios = require("axios");
-const jsdom = require("jsdom");
-
-axios.default.post("https://www.notion.so/api/v3/getAssetsJsonV2", {
-    hash: ""
-}).then(v => {
-    console.log(v.data.localeHtml, v.data.localeHtml['ko-KR']);
-    const KrDom = jsdom.JSDOM.fromURL("https://www.notion.so" + v.data.localeHtml['ko-KR']).then(v => {
-        const newKr = JSON.parse(v.window.document.getElementById("messages").textContent)
-        console.log(newKr)
-    });
-    const zhDom = jsdom.JSDOM.fromURL("https://www.notion.so" + v.data.localeHtml['zh-CN']).then(v => {
-        const newZh = console.log(v.window.document.getElementById("messages").textContent)
-        console.log(newZh)
-    });
-});
-
+require("./notion-zh_CN");
