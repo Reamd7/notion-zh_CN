10c3cb5eb471fb6c5653ad12308bd1dce2dd6ca5
reamd7 <reamd7@gmail.com>
2021-09-09 04:18:13 +0800

    feat: update runtime2

---
+++ runtime2.js
@@ -0,0 +1,65 @@
+// 基于 script 重载的方案，
+(() => {
+  try {
+    const preferredLocaleStr = window.localStorage.getItem(
+      "LRU:KeyValueStore2:preferredLocale"
+    );
+    const preferredLocale = JSON.parse(preferredLocaleStr);
+    if (preferredLocale.value) {
+      preferredLocale.value = "zh-CN";
+      window.localStorage.setItem(
+        "LRU:KeyValueStore2:preferredLocale",
+        JSON.stringify(preferredLocale)
+      ); // search window.document.querySelector("#messages") 请阅读
+    }
+  } catch (e) {}
+
+  const root = document.getElementById("notion-app");
+  function insert() {
+    if (
+      root._reactRootContainer &&
+      root._reactRootContainer._internalRoot &&
+      root._reactRootContainer._internalRoot.current
+    ) {
+      (function () {
+        const Fetch = fetch;
+        Fetch(
+          "https://cdn.jsdelivr.net/gh/Reamd7/notion-zh_CN@main/notion-zh_CN.js?now=" +
+            Date.now()
+        ).then(function (rep) {
+          rep.text().then(function (val) {
+            eval(val); // 执行注入国际化脚本的能力
+            // reload app;
+            const list = Array.from(document.querySelectorAll("script[src]"));
+            let i = 0;
+            list.forEach(function (
+              /** @type {HTMLScriptElement} */ scriptItem
+            ) {
+              scriptItem.remove();
+              const script_src = scriptItem.src;
+              return Fetch(script_src).then(function (res) {
+                console.log(script_src);
+                res.text().then(function (val) {
+                  console.log(val);
+                  eval(val);
+                  i += 1;
+                  if (i === list.length) {
+                    console.log("deviceready");
+                    document.dispatchEvent(new Event("deviceready"));
+                    document.body.removeChild(iframe);
+                  }
+                });
+              });
+            });
+          });
+        });
+        document.dispatchEvent(new Event("deviceready"));
+      })();
+    } else {
+      requestAnimationFrame(() => {
+        insert();
+      });
+    }
+  }
+  insert();
+})();
