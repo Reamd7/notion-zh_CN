cb91e368da4951ad83278234097380d699ac21da
reamd7 <reamd7@gmail.com>
2021-09-09 17:05:51 +0800

    feat: fix android2

--- apk/android2.js
+++ apk/android2.js
@@ -4,11 +4,6 @@
       return rep.text();
     });
   };
-  const loading = document.getElementById("initial-loading-spinner");
-  let loadingEl = null;
-  if (loading) {
-    loadingEl = loading.cloneNode();
-  }
   Fetch(
     "https://cdn.jsdelivr.net/gh/Reamd7/notion-zh_CN@main/notion-zh_CN.js?now=" +
       Date.now()
@@ -22,7 +17,6 @@
     const root = document.createElement("div");
     root.setAttribute("id", "notion-app");
     document.body.appendChild(root);
-    loadingEl && document.body.appendChild(loadingEl);
     const list = Array.from(document.querySelectorAll("script[src]")).filter(
       function (scriptItem) {
         return scriptItem.src.includes("www.notion.so");
@@ -31,25 +25,23 @@
     let i = 0;
     list.forEach(function (scriptItem) {
       const script_src = scriptItem.src;
-      return (
-        console.log(script_src);
-        Fetch(script_src).then(function (text) {
-          eval(text);
-          i += 1;
-          if (i === list.length) {
-            function insert() {
-              console.log("deviceready insert");
-              root._reactRootContainer
-                ? requestAnimationFrame(function () {
-                    insert();
-                  })
-                : document.dispatchEvent(new Event("deviceready"));
-            }
-            console.log("deviceready");
-            insert();
+      console.log(script_src);
+      return Fetch(script_src).then(function (text) {
+        eval(text);
+        i += 1;
+        if (i === list.length) {
+          function insert() {
+            console.log("deviceready insert");
+            root._reactRootContainer
+              ? requestAnimationFrame(function () {
+                  insert();
+                })
+              : document.dispatchEvent(new Event("deviceready"));
           }
-        })
-      );
+          console.log("deviceready");
+          insert();
+        }
+      });
     });
   });
 })();
