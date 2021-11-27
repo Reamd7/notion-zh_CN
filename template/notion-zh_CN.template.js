cbbf40f2dc3a4600cbcb59787b3a0cf6ce5bd891
reamd7 <reamd7@gmail.com>
2021-11-28 01:21:48 +0800

    feat: 增加修复中文版时间格式的问题

--- template/notion-zh_CN.template.js
+++ template/notion-zh_CN.template.js
@@ -64,4 +64,27 @@
     }
     insert();
   }
+  function insertMoment () {
+    try {
+      moment.updateLocale("zh-cn", {
+        longDateFormat: {
+          LT: 'h:mm A',
+          LTS: 'h:mm:ss A',
+          L: 'YYYY/MM/DD',
+          LL: 'YYYY年M月D日',
+          LLL: 'YYYY年M月D日Ah点mm分',
+          LLLL: 'YYYY年M月D日ddddAh点mm分',
+          l: 'YYYY/M/D',
+          ll: 'YYYY年M月D日',
+          lll: 'YYYY年M月D日 HH:mm',
+          llll: 'YYYY年M月D日dddd HH:mm',
+        },
+      })
+    } catch(e) {
+      requestAnimationFrame(() => {
+        insertMoment();
+      })
+    }
+  }
+  insertMoment();
 })();
