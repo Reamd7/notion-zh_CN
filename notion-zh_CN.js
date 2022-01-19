92dff9872aed662cf2917fc830ba35d16db12084
reamd7 <reamd7@gmail.com>
2022-01-20 00:41:17 +0800

    feat: 2.4.0 支持 userScript 插件 —— ios safari 也可使用了

--- notion-zh_CN.js
+++ notion-zh_CN.js
@@ -1,17 +1,26 @@
 // ==UserScript==
 // @name         Notion-zh_CN notion的汉化脚本
 // @namespace    http://tampermonkey.net/
-// @version      2.3.9
-// @description  notion的100%汉化脚本，基于官方中文+机器翻译韩文，支持app版本以及网页油猴，地址：https://github.com/reamd7/notion-zh_CN
+// @version      2.4.0
+// @description  notion的100%汉化脚本，基于官方中文+机器翻译韩文，支持app版本以及网页油猴/UserScript，地址：https://github.com/reamd7/notion-zh_CN
 // @author       reamd7
 // @match        *://www.notion.so/*
 // @grant        none
 // @run-at       document-start
-// @copyright    2021, reamd7
+// @copyright    2021-2022, reamd7
 // @license      MIT
 // ==/UserScript==
 (function () {
   "use strict";
+  var isSafari = navigator.userAgent.includes('Safari/') && !navigator.userAgent.includes('Chrome/')
+  var isElectron = "undefined" != typeof global || window.__isElectron;
+
+  const scriptList = document.querySelectorAll(('script[defer]'));
+  const scriptSrcList = Array.from(scriptList).map(v => v.src)
+  if (isSafari) {
+    scriptList.forEach(v => v.remove())
+    document.getElementById("notion-app").remove();
+  }
 
   const script = document.createElement("script");
   script.id = "messages";
@@ -8034,50 +8043,6 @@
   routes.setAttribute("data-locale", "zh-CN");
   routes.text = JSON.stringify({});
 
-  try {
-    const preferredLocaleStr = window.localStorage.getItem(
-      "LRU:KeyValueStore2:preferredLocale"
-    );
-    const preferredLocale = JSON.parse(preferredLocaleStr);
-    if (preferredLocale.value) {
-      preferredLocale.value = "zh-CN";
-      window.localStorage.setItem(
-        "LRU:KeyValueStore2:preferredLocale",
-        JSON.stringify(preferredLocale)
-      ); // search window.document.querySelector("#messages") 请阅读
-    }
-  } catch (e) {}
-  var isElectron = "undefined" != typeof global || window.__isElectron;
-  if (isElectron) {
-    var observer = new MutationObserver(function (callback) {
-      if (
-        callback.filter((v) => {
-          return v.target === document.head;
-        }).length > 0
-      ) {
-        document.head.insertAdjacentElement("afterbegin", script);
-        document.head.insertAdjacentElement("afterbegin", routes);
-        observer.disconnect();
-      }
-    });
-    observer.observe(document, {
-      childList: true, // 观察目标子节点的变化，是否有添加或者删除
-      attributes: false, // 观察属性变动
-      subtree: true, // 观察后代节点，默认为 false
-    });
-  } else {
-    function insert() {
-      try {
-        document.body.appendChild(script);
-        document.body.appendChild(routes);
-      } catch (e) {
-        requestAnimationFrame(() => {
-          insert();
-        });
-      }
-    }
-    insert();
-  }
   function insertMoment() {
     try {
       moment.updateLocale("zh-cn", {
@@ -8100,5 +8065,64 @@
       });
     }
   }
-  insertMoment();
+  try {
+    const preferredLocaleStr = window.localStorage.getItem(
+      "LRU:KeyValueStore2:preferredLocale"
+    );
+    const preferredLocale = JSON.parse(preferredLocaleStr);
+    if (preferredLocale.value) {
+      preferredLocale.value = "zh-CN";
+      window.localStorage.setItem(
+        "LRU:KeyValueStore2:preferredLocale",
+        JSON.stringify(preferredLocale)
+      ); // search window.document.querySelector("#messages") 请阅读
+    }
+  } catch (e) {}
+  if (isElectron) {
+    var observer = new MutationObserver(function (callback) {
+      if (
+        callback.filter((v) => {
+          return v.target === document.head;
+        }).length > 0
+      ) {
+        document.head.insertAdjacentElement("afterbegin", script);
+        document.head.insertAdjacentElement("afterbegin", routes);
+        observer.disconnect();
+      }
+    });
+    observer.observe(document, {
+      childList: true, // 观察目标子节点的变化，是否有添加或者删除
+      attributes: false, // 观察属性变动
+      subtree: true, // 观察后代节点，默认为 false
+    });
+    insertMoment();
+  } else {
+    function insert() {
+      try {
+        document.body.appendChild(script);
+        document.body.appendChild(routes);
+      } catch (e) {
+        requestAnimationFrame(() => {
+          insert();
+        });
+      }
+    }
+    insert();
+    insertMoment();
+
+    if (isSafari) {
+      const notionRoot = document.createElement('div');
+      notionRoot.id = "notion-app"
+      notionRoot.setAttribute("data", 1);
+      document.body.append(notionRoot);
+      scriptSrcList.forEach(url => {
+        const script = document.createElement("script");
+        script.type= 'text/javascript';
+        script.defer = "defer";
+        script.src = url;
+        script.setAttribute("inject")
+        document.head.append(script)
+      })
+    }
+  }
 })();
