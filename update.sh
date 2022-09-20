d53b25c4ea1ffe6fee6e8d4ef1a2061d72d1ed70
某亚瑟 <mouyase@qq.com>
2022-09-20 11:50:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 folder="/Applications/Notion.app/Contents/Resources/app/renderer"
-preloadJs="/Applications/Notion.app/Contents/Resources/app/renderer/preload.js"
-remoteUrl="https://greasyfork.org/scripts/430116-notion-zh-cn-notion%E7%9A%84%E6%B1%89%E5%8C%96%E8%84%9A%E6%9C%AC/code/Notion-zh_CN%20notion%E7%9A%84%E6%B1%89%E5%8C%96%E8%84%9A%E6%9C%AC.user.js"
+preloadJs="$folder/preload.js"
+remoteUrl="https://github.com/Reamd7/notion-zh_CN/releases/latest/download/notion-zh_CN.js"
 
-if [ -w "/Applications/Notion.app/Contents/Resources/app/renderer/preload.js" ]; then
-    curl -# -o "$folder/Notion-zh_CN.js" "$remoteUrl"
-
-    listLine="tail -n 1 $preloadJs"
-
-    if [ "$listLine" != "require('./notion-zh_CN')" ]; then 
-      echo "require('./notion-zh_CN')" >> "$preloadJs"
+if [ -w "$preloadJs" ]; then
+    curl -L -# -o "$folder/notion-zh_CN.js" "$remoteUrl"
+    listLine=$(tail -n 1 "$preloadJs") 
+    if [ "$listLine" != "require(\"./notion-zh_CN\")" ]; then 
+      echo "require(\"./notion-zh_CN\")" >> "$preloadJs"
     fi
 fi
