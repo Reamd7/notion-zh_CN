a33ecb2e45af5d1b10337b081f8967defabc2ef0
某亚瑟 <mouyase@qq.com>
2023-02-01 04:14:09 +0800

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,7 @@
-Invoke-WebRequest -Uri "https://greasyfork.org/zh-CN/scripts/430116-notion-%E5%AE%8C%E5%85%A8%E4%B8%AD%E6%96%87%E5%8C%96-%E5%9F%BA%E4%BA%8E%E9%9F%A9%E8%AF%AD%E7%89%88%E6%9C%AC-%E4%BD%BF%E7%94%A8%E8%85%BE%E8%AE%AFapi%E6%9C%BA%E7%BF%BB" -OutFile "$HOME\AppData\Local\Programs\Notion\resources\app\renderer\notion-zh_CN.js"[string]$content=Get-Content "$HOME\AppData\Local\Programs\Notion\resources\app\renderer\preload.js"If(!$content.Contains("notion-zh_CN"))
+Invoke-WebRequest -Uri "https://greasyfork.org/scripts/430116-notion-zh-cn-notion%E7%9A%84%E6%B1%89%E5%8C%96%E8%84%9A%E6%9C%AC/code/Notion-zh_CN%20notion%E7%9A%84%E6%B1%89%E5%8C%96%E8%84%9A%E6%9C%AC.user.js
+" -OutFile "$HOME\AppData\Local\Programs\Notion\resources\app\renderer\notion-zh_CN.js"
+[string]$content=Get-Content "$HOME\AppData\Local\Programs\Notion\resources\app\renderer\preload.js"
+If(!$content.Contains("notion-zh_CN"))
 {
 Add-Content "$HOME\AppData\Local\Programs\Notion\resources\app\renderer\preload.js" `n'require("./notion-zh_CN")'
-}
+}
