750190529492ca7c40c11de5cd7574239e9b0b13
reamd7 <reamd7@gmail.com>
2023-09-17 23:25:16 +0800

    feat: 完成asar 文件的解压以及文件的注入

--- update_asar/src/main.rs
+++ update_asar/src/main.rs
@@ -1,18 +1,17 @@
-use std::path::Path; 
+use std::path::{Path, PathBuf}; 
 use std::fs;
 use asar::{AsarReader};
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let notion_install_path = Path::new("C:\\Users\\fdzm\\AppData\\Local\\Programs\\Notion");
+    let notion_install_path = Path::new("/mnt/c/Users/Gemini/AppData/Local/Programs/Notion");
 
-    let notion_resource_path = notion_install_path.join("./resources");
+    let notion_resource_path = notion_install_path.join("resources");
 
-    let notion_app_asar = notion_resource_path.join("./app.asar");
-    let app_asar_copy = notion_resource_path.join("./app.asar.backup");
-
-    let app_asar_is_file = notion_app_asar.is_file();
-    if app_asar_is_file {
+    let notion_app_asar = notion_resource_path.join("app.asar");
+    let app_asar_copy = notion_resource_path.join("app.asar.backup");
+    println!("{:?} is {}", notion_app_asar, notion_app_asar.is_file());
+    if notion_app_asar.is_file() {
         // 先备份
         fs::copy(&notion_app_asar, &app_asar_copy)?;
         fs::remove_file(&notion_app_asar)?;
@@ -20,23 +19,34 @@ fn main() -> Result<()> {
     if app_asar_copy.is_file() {
         let asar_file = fs::read(&app_asar_copy)?;
         let asar = AsarReader::new(
-            &asar_file, app_asar_copy
+            &asar_file, notion_app_asar.clone()
         )?;
 
         if !notion_app_asar.is_dir() {
             fs::create_dir(&notion_app_asar)?;
-
             // unzip
-            for path in asar.files().keys() {
-                println!("{}", path.display());
+            for file_path in asar.files().keys() {
+                let file = asar.files().get(file_path).unwrap();
+                
+                let extra_path = notion_app_asar.join(file_path);
+                let extra_parent_path = extra_path.parent();
+                if let Some(extra_parent_path) = extra_parent_path {
+                    if !extra_parent_path.is_dir() {
+                        if let Err(err) = fs::create_dir_all(extra_parent_path) {
+                            println!("CreateDirError {:?}", err);
+                        }
+                    }
+                }
+                // 涉及系统平台的内容不一定是标准的utf-8 格式，所以是不可以直接转换的
+                let content = file.data();
+                fs::write(extra_path, content)?;
             }
-            fs::write(path, contents)
         }
 
         println!("There are {} files in archive.asar", asar.files().len());
         
         // let files = asar.files().keys().collect::<Vec<_>>();
-        let preload_path = Path::new("renderer\\preload.js");
+        let preload_path = Path::new("renderer/preload.js");
         let preload_js_file = asar.files().get(preload_path).unwrap();
         let preload_js_contents = std::str::from_utf8(preload_js_file.data()).unwrap();
 
@@ -46,7 +56,7 @@ fn main() -> Result<()> {
         )?;
 
         let i18n_content = "alert(1)";
-        let i18n_path = Path::new("renderer\\notion-zh_CN.js");
+        let i18n_path = Path::new("renderer/notion-zh_CN.js");
 
         fs::write(
             notion_app_asar.join(i18n_path),
