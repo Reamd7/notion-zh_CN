f62580e78b3a8928654cf6d3e36c502e32a1caac
reamd7 <reamd7@gmail.com>
2023-09-22 11:13:22 +0800

    feat: fmt file & 提取寻找 Notion 安装目录

--- update_asar/src/main.rs
+++ update_asar/src/main.rs
@@ -1,9 +1,9 @@
-use std::path::{Path, PathBuf}; 
-use std::fs;
-use asar::{AsarReader};
 use anyhow::Result;
-use toml_edit::Document;
+use asar::AsarReader;
 use directories::BaseDirs;
+use std::fs;
+use std::path::{Path, PathBuf};
+use toml_edit::Document;
 
 struct UpdateScriptEnv {
     version: String,
@@ -20,29 +20,21 @@ enum URL {
 impl UpdateScriptEnv {
     fn new() -> UpdateScriptEnv {
         let toml_path = Path::new("env.toml");
-        let toml = String::from_utf8(
-            fs::read(toml_path).unwrap()
-        ).unwrap();
+        let toml = String::from_utf8(fs::read(toml_path).unwrap()).unwrap();
 
         let doc = toml.parse::<Document>().unwrap();
         UpdateScriptEnv {
             version: doc["version"].as_str().unwrap().to_string(),
             folder: match doc["folder"].as_str() {
-                Some("~") => if let Some(d) = BaseDirs::new() {
-                    let mut x = String::from(d.data_local_dir().to_str().unwrap());
-                    x.push_str("/Programs/Notion");
-                    x
-                }else{ panic!() },
+                Some("~") => UpdateScriptEnv::find_notion_install_path(),
                 Some(dir) => dir.to_string(),
-                None => panic!()
+                None => panic!(),
             },
             url: match doc.get("local_url") {
-                Some(local) => Some(URL::Local(
-                    match local.as_str() {
-                        Some(s)=>s.to_string(),
-                        None => panic!()
-                    }
-                )),
+                Some(local) => Some(URL::Local(match local.as_str() {
+                    Some(s) => s.to_string(),
+                    None => panic!(),
+                })),
                 None => match doc.get("remote_url") {
                     Some(remote) => Some(URL::Remote(remote.as_str().unwrap().to_string())),
                     None => None,
@@ -54,7 +46,17 @@ impl UpdateScriptEnv {
     pub fn update_version(&mut self, next_version: &str) -> () {
         self.__doc__["version"] = toml_edit::value(next_version);
         self.version = next_version.to_string();
-    } 
+    }
+    
+    fn find_notion_install_path() -> String {
+        if let Some(d) = BaseDirs::new() {
+            let mut x = String::from(d.data_local_dir().to_str().unwrap());
+            x.push_str("/Programs/Notion");
+            x
+        } else {
+            panic!()
+        }
+    }
 }
 
 #[tokio::main]
@@ -76,16 +78,14 @@ async fn main() -> Result<()> {
     }
     if app_asar_copy.is_file() {
         let asar_file = fs::read(&app_asar_copy)?;
-        let asar = AsarReader::new(
-            &asar_file, notion_app_asar.clone()
-        )?;
+        let asar = AsarReader::new(&asar_file, notion_app_asar.clone())?;
 
         if !notion_app_asar.is_dir() {
             fs::create_dir(&notion_app_asar)?;
             // unzip
             for file_path in asar.files().keys() {
                 let file = asar.files().get(file_path).unwrap();
-                
+
                 let extra_path = notion_app_asar.join(file_path);
                 let extra_parent_path = extra_path.parent();
                 if let Some(extra_parent_path) = extra_parent_path {
@@ -108,8 +108,8 @@ async fn main() -> Result<()> {
         let preload_js_contents = std::str::from_utf8(preload_js_file.data()).unwrap();
 
         fs::write(
-            notion_app_asar.join(preload_path), 
-            format!("{}\n{}", preload_js_contents, "require('./Notion-zh_CN');")
+            notion_app_asar.join(preload_path),
+            format!("{}\n{}", preload_js_contents, "require('./Notion-zh_CN');"),
         )?;
 
         let text: String = match &env.url {
@@ -134,10 +134,7 @@ async fn main() -> Result<()> {
 
         let i18n_path = Path::new("renderer/notion-zh_CN.js");
 
-        fs::write(
-            notion_app_asar.join(i18n_path),
-            text
-        )?;
+        fs::write(notion_app_asar.join(i18n_path), text)?;
 
         println!("完成 i18n 注入");
     }
